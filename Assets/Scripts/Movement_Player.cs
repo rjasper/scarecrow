64a45da29afbf0077800d4436b31651af63de0ae
Huy Vu <huy-vu@gmx.de>
2016-09-05 18:14:58 +0200

    new player prefab and script folder

---
+++ Assets/Scripts/Movement_Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_Player : MonoBehaviour {
+
+	
+	void Start () {
+	
+	}
+	//using arrow buttons or WASD for player character movements
+	void Update () {
+        Transform t = GetComponent<Transform>();
+
+        if ((Input.GetKeyDown("right")) || (Input.GetKeyDown(KeyCode.D)))
+        {
+            t.Translate (new Vector2(1, 0));
+        }
+
+        if ((Input.GetKeyDown("left")) || (Input.GetKeyDown(KeyCode.A)))
+                {
+            t.Translate (new Vector2(-1, 0));
+        }
+
+        if ((Input.GetKeyDown("up")) || (Input.GetKeyDown(KeyCode.W)))
+        {
+            t.Translate(new Vector2(0, 1));
+        }
+
+        if ((Input.GetKeyDown("down")) || (Input.GetKeyDown(KeyCode.S)))
+        {
+            t.Translate(new Vector2(0, -1));
+        }
+    }
+}
