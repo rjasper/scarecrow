ebfbb8214605a3fd15013433c618589ea8e25253
Huy Vu <huy-vu@gmx.de>
2016-09-06 17:23:28 +0200

    writing follow script for enemy

--- Assets/Scripts/Stalking.cs
+++ Assets/Scripts/Stalking.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class Stalking : MonoBehaviour {
+    public GameObject player;
+    public float speed = 1f;
+    private Rigidbody2D rb;
 
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// search and get near the position of player character
-	void Update () {
-	
+	void FixedUpdate () {
+        Transform t = GetComponent<Transform>();
+        Vector2 direction = player.transform.position - t.position;
 
-	}
+        rb.velocity = speed * direction.normalized;
+    }
 }
