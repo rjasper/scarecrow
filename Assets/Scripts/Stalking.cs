7fd27dcf12802530034d5e89b8ab8da8245c8433
Huy Vu <huy-vu@gmx.de>
2016-09-06 16:16:53 +0200

    creating enemy prefab

---
+++ Assets/Scripts/Stalking.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stalking : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	// search and get near the position of player character
+	void Update () {
+	
+
+	}
+}
