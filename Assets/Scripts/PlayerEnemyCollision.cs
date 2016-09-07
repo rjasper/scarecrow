9b1daad14cc6d08ac46e89375ad24052be7973d1
Rico Jasper <jasper.rico@googlemail.com>
2016-09-07 11:43:37 +0200

    enemy collision detection

---
+++ Assets/Scripts/PlayerEnemyCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEnemyCollision : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D other = collision.collider;
+
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy Contact");
+
+            // TODO trigger player death
+        }
+    }
+
+}
