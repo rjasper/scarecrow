be05df2ada678f8067b162cc75f7d65c9d2508a8
Huy Vu <huy-vu@gmx.de>
2016-09-07 21:05:46 +0200

    corrected trigger script

--- Assets/Scripts/Stalking.cs
+++ Assets/Scripts/Stalking.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Stalking : MonoBehaviour {
+public class Stalking : MonoBehaviour
+{
 	public GameObject player;
 	public float speed = 1f;
 	private Rigidbody2D rb;
 
-	void Start () {
-		rb = GetComponentInParent<Rigidbody2D>();
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// search and get near the position of player character
-	void OnCollisionStay2D (Collision2D coll) {
-		if (coll.gameObject.tag == "Enemy")
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		if (coll.CompareTag("Player"))
 		{
-			Transform t = GetComponentInParent<Transform>();
+			Transform t = GetComponent<Transform>();
 			Vector2 direction = player.transform.position - t.position;
 
 			rb.velocity = speed * direction.normalized;
 		}
 	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		if (coll.CompareTag("Player"))
+			rb.velocity = Vector2.zero;
+	}
+
 }
