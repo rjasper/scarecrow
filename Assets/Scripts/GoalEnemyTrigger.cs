f25916e1985df0ea69c4d12f4959be637afeb4e8
Rico Jasper <jasper.rico@googlemail.com>
2016-09-08 12:01:32 +0200

    goal enemy trigger

---
+++ Assets/Scripts/GoalEnemyTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalEnemyTrigger : MonoBehaviour {
+
+	private ColorState colorState;
+
+	void Start () {
+		colorState = GetComponent<ColorState>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.CompareTag("Enemy") && !collider.isTrigger)
+		{
+			Debug.Log("enemy triggered goal");
+		}
+	}
+
+}
